4ed012e8aedd6104336ddbec3264ff47cd3b40e6
GregorGo <95254972+GregorGo@users.noreply.github.com>
2022-07-28 11:56:24 +0300

    Delete name = 'robb'.py

--- .vscode/name = 'robb'.py
+++
@@ -1,6 +0,0 @@
-name = 'robb'
-print(name.lower())  # => robb
-num1 = 5 
-num2 = 3
-# bit_length() - вычисляет количество бит, необходимых для представления числа в двоичном виде
-print(num1.bit_length() + num2.bit_length())  # => 8
