bcb46195cc19e30a111b82833a7900537cf20f91
Grigoriy <mr.igoshkin@inbox.ru>
2022-07-28 12:09:41 +0300

    add in cli and brain

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -1,11 +1,6 @@
 import prompt
 
 def welcome_user():
-<<<<<<< HEAD
 	name = prompt.string('May I have your name? ')	
-	print(name)
-welcome_user()
-=======
-	
-print('May I have your name? ')
->>>>>>> c03283d192a754c500c64122bea772a0d2f65849
+	print(f"Hello, {name} !")
+
--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -4,11 +4,8 @@ from brain_games.cli import welcome_user
 
 def main():
     print("Welcome to the Brain Games!")
-welcome_user()    
-if __name__ == '__main__':
-<<<<<<< HEAD
-    main()
-=======
-    main()
+welcome_user() 
 
->>>>>>> c03283d192a754c500c64122bea772a0d2f65849
+
+if __name__ == '__main__':
+    main()
