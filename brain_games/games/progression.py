acf1c9c3e0f81aee2ba45937dff1a2e946c692cf
Grigoriy <mr.igoshkin@inbox.ru>
2022-11-08 00:01:46 +0300

    add progression

---
+++ brain_games/games/progression.py
@@ -0,0 +1,30 @@
+import prompt
+
+from random import choice, randint
+
+
+def random_game():
+    """Find the number of missing in the progression."""
+    print('Welcome to the Brain Games!')
+    name = prompt.string('May I have your name? ')
+    print('Hello, {0}!'.format(name))
+    print('What number is missing in the progression?')
+    counter = 0
+    while counter < 3:
+        step = randint(5, 10)
+        num1 = randint(1, 5)
+        num2 = randint(55, 60)
+        numbers = tuple(range(num1, num2, step))
+        string = " ".join(map(str, numbers))
+        number = str(choice(numbers))
+        num_of_repl = string.replace(number, '..', 1)
+        print('Question: {}'.format(num_of_repl))
+        result = number
+        char = prompt.string('Your answer: ')
+        if str(char) == str(result):
+            print('Correct!')
+        else:
+            print('"{}" is wrong answer ;(. Correct answer was "{}".'.format(char, result))
+            return print("Let\'s try again, {0}!".format(name))
+        counter += 1
+    print('Congratulations, {0}!'.format(name))
---
+++ brain_games/scripts/brain_progression.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Script to run."""
+
+from brain_games.games.progression import random_game
+
+
+def main():
+    """Play a calculator game."""
+    random_game()
+
+
+if __name__ == '__main__':
+    main()
