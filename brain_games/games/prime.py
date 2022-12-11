33e041acf9933de159d69b0b42c435c8d447c324
Grigoriy <mr.igoshkin@inbox.ru>
2022-12-11 21:07:11 +0300

    fix prime

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -13,24 +13,21 @@ def random_game():
     print('Answer "yes" if given number is prime. Otherwise answer "no".')
     counter = 0
     while counter < 3:
-        num = 3
-        account = 0
-        if num == 1 or num == 2 or num == 3:
-            result = 'yes'
-        for i in range(2, num // 2 + 1):
-            if (num % i == 0):
-                account += 1
-            if (account <= 0):
-                result = 'yes'
+        number_random = randint(2, 100)
+        divider = 2
+        while divider <= (number_random // 2):
+            if number_random % divider == 0:
+                result = "no"
+                break
             else:
-                result = 'no'
-        print('Question: {0}'.format(num))
+                divider += 1
+            result = "yes"
+        print('Question: {0}'.format(number_random))
         char = prompt.string('Your answer: ')
         if str(char) == str(result):
             print('Correct!')
         else:
-            print('''"{}" is wrong answer ;(. 
-            Correct answer was "{}".'''.format(char, result))
+            print('''"{}" is wrong answer ;(. Correct answer was "{}".'''.format(char, result))
             return print("Let\'s try again, {0}!".format(name))
         counter += 1
     print('Congratulations, {0}!'.format(name))
